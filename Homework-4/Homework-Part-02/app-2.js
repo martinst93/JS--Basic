876cac5f51bf06dec6cd84badf10c5dc8577bfcd
martinst93 <stojanov003@gmail.com>
2021-01-13 00:03:49 +0100

    Update app-2.js

--- Homework-4/Homework-Part-02/app-2.js
+++ Homework-4/Homework-Part-02/app-2.js
@@ -1,14 +1,17 @@
 const fiveNumbers = [1, 2, 3, 4, 5];
 
 
-function sumOfNumbers(num){ return console.log((num[0] + num[1] + num[2] + num[3] + num[4])) };
+function sumOfNumbers(num){
+      return console.log((num[0] + num[1] + num[2] + num[3] + num[4]))
+ };
 
 
 
 // BONUS 
 
 function validateNumber(){
-   let res = fiveNumbers.every(function(number) {return typeof number === 'number';});
+   let res = fiveNumbers.every(function(number) {
+        return typeof number === 'number';});
 
    if(res) 
         sumOfNumbers(fiveNumbers);
