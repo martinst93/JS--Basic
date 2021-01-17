97d20da51f0f865944cdf40a29cabaf2edc007e9
martinst93 <77295064+martinst93@users.noreply.github.com>
2021-01-17 22:06:20 +0100

    Add files via upload

---
+++ Homework-5/Homework-Part-02/main.js
@@ -0,0 +1,26 @@
+// for ( i = 1; i < 20; i++){
+//     if ( i % 2 == 0 ){
+//         console.log(`${i} \n `);
+//     } else {
+//         console.log(`${i} " "`);
+//     }
+// }
+
+
+
+// for (let number = 1; number <= 20; number++) {
+//     if(number % 2 == 0) {
+//       console.log(+ number + '\n');
+//     } else {
+//       console.log(" ");
+//     }  
+// }
+
+
+
+
+for (let i=1; i<= 20; i=i+2) {
+    console.log(i + ' ' + (i + 1))
+  }
+  
+  
